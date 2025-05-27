a042833306e579a8dfed3fa1ffead456534ece5f
semenyaka08 <Serh.semka@gmail.com>
2025-05-27 11:57:02 +0300

    Fix Length method to pass the Unit tests

--- src/DataStructure/Implementation/DoublyLinkedCharacterList.cs
+++ src/DataStructure/Implementation/DoublyLinkedCharacterList.cs
@@ -17,7 +17,7 @@ public class DoublyLinkedCharacterList : BaseList
 
     public override int Length()
     {
-        return _count + 52;
+        return _count;
     }
     
     public override void Append(char element)
